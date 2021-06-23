abbd1d96a78981f1b099f41b56f1256887b0fa78
ptatro <6535799+ptatro@users.noreply.github.com>
2021-06-22 21:31:23 -0500

    DB setup and tests started

--- ormQueries/products/product_crud.py
+++ ormQueries/products/product_crud.py
@@ -3,4 +3,12 @@ from .models import Product
 class ProductCrud:
     @classmethod
     def get_all_products(cls):
-        pass
+        return Product.objects.all()
+
+    @classmethod
+    def find_by_model(cls, model):
+        return Product.objects.get(model='Heavy Duty Steel Clock')
+    
+    @classmethod
+    def last_record():
+        return 
